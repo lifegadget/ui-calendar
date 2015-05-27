f9cd43fe97d03f0d691159bebc4b918723be87e2
Ken Snyder <ken@ken.net>
2015-05-27 22:53:29 +0100

    font-family, am/pm, stopTime versus duration

--- addon/components/mini-datetime.js
+++ addon/components/mini-datetime.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
+import moment from 'moment';
 const { computed, observer, $, A, run, on, typeOf, debug, keys, get, set, inject } = Ember;    // jshint ignore:line
+const htmlSafe = Ember.String.htmlSafe;
+const styleProperties = ['_styleFontFamily', '_styleMaxWidth'];
 
 import layout from '../templates/components/mini-datetime';
 
@@ -7,28 +10,87 @@ export default Ember.Component.extend({
   layout: layout,
   classNames: ['ui-calendar', 'mini-datetime', 'flexy'],
   classNameBindings: ['_size'],
+  attributeBindings: ['_style:style'],
   value: null,
   size: null,
+  ampm: true,
+  _timeFormat: computed('ampm', function() {
+    return this.get('ampm') ? 'h:mm' : 'H:mm';
+  }),
   _size: on('init',computed('size', function() {
     const size = this.get('size');
     return size === 'default' ? '' : `font-${size}`;
   })),
   // use either duration or stopTime, not both
   duration: null, // measured in # of minutes
+  _duration: computed('duration', function() {
+    const { duration, stopTime, value } = this.getProperties('duration','stopTime', 'value');
+    const calcDuration = (start,stop) => {
+      return Math.abs(moment(start).diff(moment(stop), 'minutes'));
+    };
+    return duration ? duration : calcDuration(value, stopTime);
+  }),
   stopTime: null,
-  // allows for explict setting of
-  useDuration: null,
-  _useDuration: computed('useDuration','duration','stopTime', function() {
-    const { useDuration, duration, stopTime } = this.getProperties('useDuration', 'duration', 'stopTime');
-    if(useDuration !== null) {
-      return useDuration;
+  _stopTime: on('init', computed('stopTime','_showDuration', function() {
+    return this.get('stopTime') && !this.get('_showDuration');
+  })),
+  _durationPretty: computed('_duration', function() {
+    const duration = this.get('_duration');
+    const hour = 60;
+    const dayThreashold = hour * 24;
+    if(duration > dayThreashold) {
+      const precise = duration % dayThreashold === 0 ? '' : '+';
+      return Math.floor(duration/dayThreashold) + precise + ' days';
+    } else if (duration % hour === 0) {
+      const inflector = duration/hour === 1 ? '' : 's';
+      return  duration / hour + ' hour' + inflector;
+    } else {
+      return  duration + ' minutes';
+    }
+  }),
+  showDuration: null,
+  _showDuration: on('init',computed('showDuration','duration','stopTime', 'value', function() {
+    const { showDuration, duration, stopTime } = this.getProperties('showDuration', 'duration', 'stopTime');
+    if(showDuration !== null) {
+      return showDuration; // UI style has been explicitly stated
     } else {
       // detect usage
       if(duration && stopTime) {
         debug('mini-datetime recieved a duration AND a stopTime; use one or the other not both.');
         return true;
+      } else if(duration && !stopTime) {
+        return true;
+      } else if(stopTime && !duration) {
+        const startTime = this.get('value');
+        const daysApart = Math.abs(moment(startTime).diff(stopTime,'days',true));
+        return daysApart > 1 ? true : false;
       }
+
+      return false;
     }
-    return this.get('duration') ? true : false;
+  })),
+  _showStopTime: computed('_showDuration','stopTime', function() {
+    const { _showDuration, stopTime } = this.getProperties('_showDuration', 'stopTime');
+    return stopTime && !_showDuration;
+  }),
+  // STYLE ATTRIBUTES
+  font: 'inherit',
+  _styleFontFamily: computed('font', function() {
+    const font = this.get('font');
+    return font ? `font-family: ${font}` : '';
+  }),
+  maxWidth: null,
+  _styleMaxWidth: computed('maxWidth', function() {
+    const maxWidth = this.get('maxWidth');
+    return maxWidth ? `max-width: ${maxWidth}` : '';
+  }),
+  // STYLER
+  _style: computed(...styleProperties,function() {
+    const styles = this.getProperties(...styleProperties);
+    const styleString = htmlSafe(keys(styles).map((key) => {
+      return styles[key];
+    }).join('; '));
+
+    return styles ? styleString : null;
   })
 });
