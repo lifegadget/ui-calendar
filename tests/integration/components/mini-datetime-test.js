6f7cedee79cb224e0fbc473d1cf4c5ffb69cb621
Ken Snyder <ken@ken.net>
2015-09-08 16:19:04 +0100

    css improvements

--- tests/integration/components/mini-datetime-test.js
+++ tests/integration/components/mini-datetime-test.js
@@ -1,5 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
+const getMinutes = m => m.diff(m.clone().startOf('day'),'minutes');
 
 moduleForComponent('mini-datetime', 'Integration | Component | mini-datetime', {
   integration: true
@@ -14,4 +16,19 @@ test('it renders', function(assert) {
   this.render(hbs`{{mini-datetime}}`);
 
   assert.equal(this.$('div').hasClass('mini-datetime'), true, 'render should have mini-datetime class');
+});
+
+test('receives a change-time action', function(assert) {
+  assert.expect(1);
+  var dateString = '2015-05-04 14:45:00';
+  var minutes = getMinutes(moment(dateString));
+
+  this.on('onTimeChange', function(newMinutes) {
+    assert.equal(newMinutes, minutes + 1, 'clicking on right chevron has increased minutes by one');
+  });
+  this.render(hbs`
+    {{mini-datetime value=dateString onTimeChange=onTimeChange}}
+  `);
+
+  this.$('.time .right').click();
 });
---
+++ tests/unit/components/mini-datetime-test.js
@@ -0,0 +1,20 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
+const { keys, create } = Object; // jshint ignore:line
+const {computed, observer, $, A, run, on, typeOf, debug, defineProperty, get, set, inject, isEmpty} = Ember;  // jshint ignore:line
+
+
+moduleForComponent('mini-datetime', 'Unit | Component | mini datetime', {
+  // Specify the other units that are required for this test
+  needs: ['helper:moment-format'],
+  unit: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(1);
+  // var component = this.subject();
+  run(()=> {
+    this.render();
+    assert.equal(this.$('div').hasClass('mini-datetime'), true, 'render should have mini-datetime class');
+  });
+});
